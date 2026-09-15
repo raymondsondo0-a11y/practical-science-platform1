f86243ee6846fe0d53303ee0789f1c5b39114c13
raymondsondo0-a11y <raymondsondo0@gmail.com>
2026-09-15 10:46:09 +0300

    Delete admin directory

--- admin/delete.php
+++
@@ -1,1 +0,0 @@
-<?php require_once __DIR__.'/../config.php';require_admin();$id=(int)($_GET['id']??0);if($id){$s=$pdo->prepare('DELETE FROM movies WHERE id=?');$s->execute([$id]);}header('Location: ./');exit;
